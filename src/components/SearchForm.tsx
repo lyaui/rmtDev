1ccabd2579d62bf975efd7950edeb787adc01ae2
Iris Huang <lyaui@outloook.com>
2024-01-19 01:53:07 +0800

    feat: add controled input

--- src/components/SearchForm.tsx
+++ src/components/SearchForm.tsx
@@ -1,15 +1,27 @@
+import { useState } from 'react';
+import type { ChangeEvent, FormEvent } from 'react';
+
 export default function SearchForm() {
+  const [serachText, setSearchText] = useState('');
+  const handleFormSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+  };
+  const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setSearchText(event.target.value);
+  };
   return (
-    <form action="#" className="search">
-      <button type="submit">
-        <i className="fa-solid fa-magnifying-glass"></i>
+    <form onSubmit={handleFormSubmit} action='#' className='search'>
+      <button type='submit'>
+        <i className='fa-solid fa-magnifying-glass'></i>
       </button>
 
       <input
-        spellCheck="false"
-        type="text"
+        value={serachText}
+        onChange={handleInputChange}
+        spellCheck='false'
+        type='text'
         required
-        placeholder="Find remote developer jobs..."
+        placeholder='Find remote developer jobs...'
       />
     </form>
   );
